e2e11c8476e3508f06272de67717cbc7b9557487
Nilesh <nileshtiwari1184@gmail.com>
2025-12-03 19:51:28 +0530

    3 December 25 Time:  7:51 PM

--- backend/api.py
+++ backend/api.py
@@ -89,4 +89,5 @@ app.include_router(subjects_router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
